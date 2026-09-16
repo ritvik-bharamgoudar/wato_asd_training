7fec6c278dabd9002a606dde6c217a09040085aa
Ritvik Bharamgoudar <ritvik.bharamgoudar@gmail.com>
2026-09-16 04:18:06 +0100

    publishing placeholder costmap

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -3,6 +3,7 @@
  
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
  
 #include "costmap_core.hpp"
  
@@ -11,12 +12,12 @@ class CostmapNode : public rclcpp::Node {
     CostmapNode();
     
     // Place callback function here
-    void publishMessage();
+    void publishCostmap();
  
   private:
     robot::CostmapCore costmap_;
     // Place these constructs here
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
  
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -5,16 +5,30 @@
  
 CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
   // Initialize the constructs and their parameters
-  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
-  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+  costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap", 10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishCostmap, this));
 }
  
 // Define the timer to publish a message every 500ms
-void CostmapNode::publishMessage() {
-  auto message = std_msgs::msg::String();
-  message.data = "Goodbye, ROS 2.";
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-  string_pub_->publish(message);
+void CostmapNode::publishCostmap() {
+  auto grid = nav_msgs::msg::OccupancyGrid();
+
+  grid.header.stamp.sec = 0;
+  grid.header.frame_id = "map";
+
+  grid.info.resolution = 0.1;
+  grid.info.width = 50;
+  grid.info.height = 50;
+  
+  grid.info.origin.position.x = 0.0;
+  grid.info.origin.position.y = 0.0;
+  grid.info.origin.position.z = 0.0;
+
+  grid.data.assign(grid.info.width * grid.info.height, 0);
+
+  //message.data = "Goodbye, ROS 2.";
+  RCLCPP_INFO(this->get_logger(), "placeholder costmap");
+  costmap_pub_->publish(grid);
 }
  
 int main(int argc, char ** argv)
